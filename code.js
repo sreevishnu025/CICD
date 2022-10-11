0d61fc51dcc73e2593a4f8c369337ff6949407c0
sreevishnu025 <60707971+sreevishnu025@users.noreply.github.com>
2022-10-11 18:58:22 +0530

    Update code.js

--- code.js
+++ code.js
@@ -3,21 +3,8 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
-
-  app.on("repository.created", async (context) => {
-    const {owner,repo} = context.repo();
-    const branch = context.payload.repository.default_branch;
-    app.log.info("A new repo has been created!!");
-    
+  async function createBranchRule(context,owner,repo,branch){
     await context.octokit.repos.updateBranchProtection({
       owner,
       repo,
@@ -32,34 +19,33 @@ module.exports = (app) => {
       require_linear_history: true,
       allow_deletions: true,
       required_conversation_resolution: true
-    })
+    });
+  }
 
-
-  });
-
-
-  app.on("create", async (context) => {
+  // Your code here
+    app.on("create", async (context) => {
     const {owner,repo} = context.repo();
     const branch = context.payload.ref;
     const ref_type = context.payload.ref_type;
-    app.log.info("branch created!!");
+    app.log.info(branch+" branch created!!");
+    app.log.info("Creating branch protection rule for "+branch);
     if(ref_type==="branch"){
-      if(branch.toLowerCase()==="release" || branch.toLowerCase()==="dev"){
-    await context.octokit.repos.updateBranchProtection({
-      owner,
-      repo,
-      branch,
-      required_status_checks: null,
-      enforce_admins: false,
-      required_pull_request_reviews: {
-        require_code_owner_reviews: true,
-        required_approving_review_count: 3
-      },
-      restrictions: null,
-      require_linear_history: true,
-      allow_deletions: true,
-      required_conversation_resolution: true
-    });
-  }}
-  } );
+      if(branch.toLowerCase()==="release"){
+        createBranchRule(context,owner,repo,branch);
+        app.log.info("Branch protection rule for "+branch+" branch has been created.");
+      }
+    else if (branch.toLowerCase()==="dev"){
+      createBranchRule(context,owner,repo,branch);
+      app.log.info("Branch protection rule for "+branch+" branch has been created.");
+
+      createBranchRule(context,owner,repo,"main");
+      app.log.info("Branch protection rule for master branch has been created.");
+      }
+    }
+
+  });
+
 };
+
+
+
